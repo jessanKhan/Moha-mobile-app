bd115961b4a9bdd2719655c3637f4ccbac17365f
za-rakib <rakib.dev70@gmail.com>
2026-02-10 16:25:03 +0600

    design some screen

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Dimensions } from 'react-native';
 import { ScaledSheet } from 'react-native-size-matters';
 import Header from '../../components/Header';
+import LinearGradient from 'react-native-linear-gradient';
+
+const { width } = Dimensions.get('window');
 
 const ProfileScreen = () => {
   return (
     <View style={styles.container}>
+      {/* Bottom Left Gradient Box */}
+      <LinearGradient
+        colors={['#E8FFD4', '#CEE1FC']}
+        style={styles.gradientBox}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      />
+
+      {/* Top Right Gradient Box */}
+      <LinearGradient
+        colors={['#E9D4FF', '#FCCEE8']}
+        style={styles.gradientBoxTop}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      />
+
       <Header
         variant="default"
         title="প্রোফাইল"
@@ -19,7 +38,9 @@ const ProfileScreen = () => {
           Manage your personal information.
         </Text>
       </View>
+
     </View>
+
   );
 };
 
@@ -45,6 +66,21 @@ const styles = ScaledSheet.create({
     color: '#6B7280',
     marginTop: '8@vs',
     fontFamily: 'July-Regular',
+  },
+  gradientBox: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: width * 0.5,
+    height: width * 0.5,
+    borderTopRightRadius: 0,
+  },
+  gradientBoxTop: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: width * 0.5,
+    height: width * 0.5,
   },
 });
 
