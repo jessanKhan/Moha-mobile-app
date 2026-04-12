4776f652a3b9addcc996315315e5c0bde8eb5042
za-rakib <rakib.dev70@gmail.com>
2026-04-12 12:05:56 +0600

    some issue fix

--- src/components/quickLinkComponent/QuickLinksComponent.tsx
+++ src/components/quickLinkComponent/QuickLinksComponent.tsx
@@ -35,9 +35,9 @@ const QuickLinksComponent: FC<QuickLinksComponentProps> = ({
                 <CustomCommonIcon
                     icon={headerIcon}
                     imageUrl={logoUrl}
-                    // bgColor="rgba(233, 10, 10, 0.2)"
+                    bgColor="rgba(255, 255, 255, 0.2)"
                     iconColor="#ffffff"
-                    size={moderateScale(20)}
+                    size={20}
                 />
                 <Text style={styles.helptxt}>{title}</Text>
             </LinearGradient>
@@ -79,12 +79,12 @@ const styles = ScaledSheet.create({
         gap: '12@ms',
     },
     helptxt: {
+        flex: 1,
         fontSize: '20@ms',
         color: 'rgba(255, 255, 255, 1)',
         fontWeight: '700',
         lineHeight: '24@ms',
         fontFamily: 'July-Bold',
-        paddingVertical: '10@ms',
     },
     linkContainer: {
         padding: '16@ms',
